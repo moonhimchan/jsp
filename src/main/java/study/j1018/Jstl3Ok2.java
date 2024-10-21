2e14919bd44dc9f1da699a3df8fa0649388710b8
moonhimchan <himchan12345@naver.com>
2024-10-21 17:53:47 +0900

    202410211753 jsp

---
+++ src/main/java/study/j1018/Jstl3Ok2.java
@@ -0,0 +1,69 @@
+package study.j1018;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/1018/Jstl3Ok2")
+public class Jstl3Ok2 extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<MemberVO> vos = new ArrayList<MemberVO>();
+		
+		MemberVO vo = null;
+		
+		vo = new MemberVO();
+		vo.setName("홍길동");
+		vo.setAge(25);
+		vo.setGender("남자");
+		vo.setJob("학생");
+		vo.setAddress("서울");
+		vos.add(vo);
+		
+		vo = new MemberVO();
+		vo.setName("김말숙");
+		vo.setAge(33);
+		vo.setGender("여자");
+		vo.setJob("의사");
+		vo.setAddress("청주");
+		vos.add(vo);
+		
+		vo = new MemberVO();
+		vo.setName("이기자");
+		vo.setAge(40);
+		vo.setGender("남자");
+		vo.setJob("판사");
+		vo.setAddress("서울");
+		vos.add(vo);
+		
+		vo = new MemberVO();
+		vo.setName("오하늘");
+		vo.setAge(60);
+		vo.setGender("여자");
+		vo.setJob("조종사");
+		vo.setAddress("진천");
+		vos.add(vo);
+		
+		vo = new MemberVO();
+		vo.setName("강감찬");
+		vo.setAge(40);
+		vo.setGender("남자");
+		vo.setJob("군인");
+		vo.setAddress("대전");
+		vos.add(vo);
+		
+		
+		request.setAttribute("vos", vos);
+		
+		String viewPage = "/study/1018_JSTL/jstl3_res2.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+}
